764928b8dca12608dafbf75bd57b60b82b5b1a0e
rimrim990 <lim990718@naver.com>
2023-10-24 21:11:05 +0900

    [1024] 자바 PubSub 학습 테스트 추가

--- src/test/java/learn/java/testrepository/spring/reactive/PubSubTest.java
+++ src/test/java/learn/java/testrepository/spring/reactive/PubSubTest.java
@@ -7,9 +7,14 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Flow.Publisher;
 import java.util.concurrent.Flow.Subscriber;
 import java.util.concurrent.Flow.Subscription;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+/**
+ * Publisher -> Subscriber
+ */
 @DisplayName("자바 PubSub 학습 테스트")
 public class PubSubTest {
 
@@ -19,7 +24,7 @@ public class PubSubTest {
         // given
         Iterable<Integer> iter = Arrays.asList(1, 2, 3, 4, 5);
 
-        final Publisher<Integer> pub = new Publisher<Integer>() {
+        final Publisher<Integer> pub = new Publisher<>() {
             @Override
             public void subscribe(Subscriber<? super Integer> subscriber) {
                 Iterator<Integer> it = iter.iterator();
@@ -28,7 +33,7 @@ public class PubSubTest {
                 subscriber.onSubscribe(new Subscription() {
                     @Override
                     public void request(long n) {
-                        while(n-- > 0) {
+                        while (n-- > 0) {
                             // 데이터가 존재하면 다음 데이터를 전송한다
                             if (it.hasNext()) {
                                 subscriber.onNext(it.next());
@@ -87,5 +92,59 @@ public class PubSubTest {
         executorService.shutdown();
 
         System.out.println(Thread.currentThread() + " EXIT");
+    }
+
+    @Test
+    @DisplayName("Stream 데이터를 가져오는 PubSub 객체 구현하기")
+    void pubsub_streamData() {
+        // given
+        Iterable<Integer> iter = Stream.iterate(1, a -> a + 1)
+            .limit(10)
+            .collect(Collectors.toList());
+
+        final Publisher<Integer> pub = sub -> sub.onSubscribe(new Subscription() {
+            @Override
+            public void request(long n) {
+                try {
+                    // 한 번의 요청에 10개의 스트림 데이터 전부 전송
+                    iter.forEach(s -> sub.onNext(s));
+                    // 데이터 전송 완료 이벤트
+                    sub.onComplete();
+                } catch (Exception ex) {
+                    // 전송 중 에러 발생 이벤트
+                    sub.onError(ex);
+                }
+            }
+
+            @Override
+            public void cancel() {
+
+            }
+        });
+
+        final Subscriber<Integer> sub = new Subscriber<>() {
+            @Override
+            public void onSubscribe(Subscription subscription) {
+                System.out.println("onSubscribe");
+                subscription.request(Long.MAX_VALUE);
+            }
+
+            @Override
+            public void onNext(Integer item) {
+                System.out.println("onNext " + item);
+            }
+
+            @Override
+            public void onError(Throwable throwable) {
+                System.out.println("onError: "+ throwable);
+            }
+
+            @Override
+            public void onComplete() {
+                System.out.println("onComplete");
+            }
+        };
+
+        pub.subscribe(sub);
     }
 }
